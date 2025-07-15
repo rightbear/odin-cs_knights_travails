a73df66b8bf58c4539fc9898b80528b986c79db6
rightbear <jjj17041113@gmail.com>
2025-07-15 21:15:34 +0800

    feat: finish vertices traversal process in function

--- functions/knightMoves.js
+++ functions/knightMoves.js
@@ -13,17 +13,13 @@ function knightMoves(start, end) {
         return null;
     }
 
-    // create an array to store the traversal
-    let result = [];
+    // queue for BFS
+    let quene = [];
 
-    // Create a queue for BFS
-    let quene= [];
-
-    // Create a 2-d array for recording visited positions, and initially mark all the vertices as not visited
+    // 2-d array for recording visited positions, and initially mark all the vertices as not visited
     let rows = 8;
     let cols = 8;
     let visited = new Array(rows);
-
     for (let i = 0; i < rows; i++) {
         visited[i] = new Array(cols);
         for (let j = 0; j < cols; j++) {
@@ -31,10 +27,42 @@ function knightMoves(start, end) {
         }
     }
 
-    // Mark source node as visited and enqueue it
-    visited[start[0]][start[1]] = true;
+    // array to keep track of the traversal to the current node (put in the start node)
+    let result = [];
 
-    return visited;
+    // mark the position of start node as visited
+    visited[start[0]][start[1]] = true;
+    // record the position of start node into the path
+    result.push(start);
+    quene.push([result, start]);
+
+    // Iterate over the queue
+    while(quene.length > 0) {
+        const current = quene.shift();
+        const result = current[0];
+        const position = current[1];
+
+        if((position[0] === end[0]) && (position[1] === end[1])) {
+            return result;
+        }
+
+        // get all adjacent vertices of the dequeued vertex current, and enquene all vertices into quene
+        const nextPositions = getNext(position, visited);
+
+        if (nextPositions.length > 0) {
+            // enquene all unvisited adjacent vertices
+            for(const nextPos of nextPositions) {
+                    // mark the position of next node as visited
+                    visited[nextPos[0]][nextPos[1]] = true;
+
+                    // record the position of next node into the path
+                    const nextResult = [...result];
+                    nextResult.push(nextPos);
+
+                    quene.push([nextResult, nextPos]);
+                }
+            }
+    }
 }
 
 // check range of coordinates is between 0 and 7
@@ -43,4 +71,26 @@ function checkPosition(coordinate) {
         return false;
     }
     return true;
+}
+
+// retrieve all unvisited positions knight can move to
+function getNext(start, visited) {
+    let nextPositions = [];
+
+    // all posible directions knight can move to
+    const moves = [
+        [2, 1], [1, 2], [-1, 2], [-2, 1],
+        [-2, -1], [-1, -2], [1, -2], [2, -1]
+    ];
+
+    for (const [dx, dy] of moves) {
+        const nextX = start[0] + dx;
+        const nextY = start[1] + dy;
+
+        if(checkPosition(nextX) && checkPosition(nextY) && !visited[nextX][nextY]) {
+            nextPositions.push([nextX, nextY]);
+        }
+    }
+
+    return nextPositions;
 }
--- functions/main.js
+++ functions/main.js
@@ -2,4 +2,8 @@
 
 import { knightMoves } from "./knightMoves.js"
 
-console.log(knightMoves([0,0],[1,2]));
+console.log(knightMoves([0,0],[1,2]));      // [ [ 0, 0 ], [ 1, 2 ] ]
+console.log(knightMoves([0,0],[3,3]));      // [ [ 0, 0 ], [ 2, 1 ], [ 3, 3 ] ]
+console.log(knightMoves([3,3],[0,0]));      // [ [ 3, 3 ], [ 1, 2 ], [ 0, 0 ] ]
+console.log(knightMoves([0,0],[7,7]));      // [ [ 0, 0 ], [ 2, 1 ], [ 4, 2 ], [ 6, 3 ], [ 7, 5 ], [ 5, 6 ], [ 7, 7 ] ]
+console.log(knightMoves([3,3],[4,3]));      // [ [ 3, 3 ], [ 5, 4 ], [ 3, 5 ], [ 4, 3 ] ]
